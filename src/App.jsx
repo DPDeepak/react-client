986e42adda0f5275f29b0e6c996cf2de95b94eda
trainode30 <deepakprajapati@successive.tech>
2019-02-18 19:14:52 +0530

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import TextFieldDemo from './pages';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <TextFieldDemo />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <TextFieldDemo />
+  </div>
+);
 
 export default App;
---
+++ src/components/slider/Slider.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Slider = (props) => {
+  const { altText, banners, defaultBanner, duration, height, random, ...rest } = props;
+
+}
